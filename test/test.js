7b1bfce985b6ed4cc571946f80bd37ebf59231d1
Colin Loretz <colin.loretz@discordapp.com>
2023-06-08 09:23:45 -0700

    Added chai and initial GET route tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,33 @@
-import assert from 'assert';
+import { expect } from 'chai';
 import { describe, it } from 'mocha';
+import server from '../src/server.js';
 
-describe('awwbot', () => {
-  it('should have tests', () => {
-    assert.ok(true);
+describe('Server', () => {
+  describe('GET /', () => {
+    it('should return a greeting message with the Discord application ID', async () => {
+      const request = {
+        method: 'GET',
+        url: new URL('/', 'http://discordo.example'),
+      };
+      const env = { DISCORD_APPLICATION_ID: '123456789' };
+
+      const response = await server.fetch(request, env);
+      const body = await response.text();
+
+      expect(body).to.equal('👋 123456789');
+    });
+  });
+
+  describe('All other routes', () => {
+    it('should return a "Not Found" response', async () => {
+      const request = {
+        method: 'GET',
+        url: new URL('/unknown', 'http://discordo.example'),
+      };
+      const response = await server.fetch(request, {});
+      expect(response.status).to.equal(404);
+      const body = await response.text();
+      expect(body).to.equal('Not Found.');
+    });
   });
 });
