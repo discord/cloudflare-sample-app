878ef13c4118fb59f5c6d966dfeeea1f9a0e0e93
caleb-chou <haloisawesome412@gmail.com>
2025-04-12 11:34:39 -0500

    minor fix

--- src/register.js
+++ src/register.js
@@ -1,4 +1,4 @@
-import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
+import { INVITE_COMMAND } from './commands.js';
 import dotenv from 'dotenv';
 import process from 'node:process';
 
@@ -34,7 +34,7 @@ const response = await fetch(url, {
     Authorization: `Bot ${token}`,
   },
   method: 'PUT',
-  body: JSON.stringify([AWW_COMMAND, INVITE_COMMAND]),
+  body: JSON.stringify([INVITE_COMMAND]),
 });
 
 if (response.ok) {
